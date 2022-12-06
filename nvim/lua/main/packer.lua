aefb8ca238082c2cb8b8f22dc53614ff50e3afa7
Thales Augusto Gelinger <thalesgelinger@192.168.1.7>
2022-12-06 10:30:26 -0300

    feat: add lspconfig

--- nvim/lua/main/packer.lua
+++ nvim/lua/main/packer.lua
@@ -14,5 +14,5 @@ return require('packer').startup(function(use)
   -- Flutter plugins
   use {'akinsho/flutter-tools.nvim', requires = 'nvim-lua/plenary.nvim'}
   use {'reisub0/hot-reload.vim'}
-
+  use 'neovim/nvim-lspconfig' -- Configurations for Nvim LSP
 end)
