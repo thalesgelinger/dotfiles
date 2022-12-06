80726bcc3811e4f722888d27140e7759f0f49200
Thales Augusto Gelinger <thalesgelinger@192.168.1.7>
2022-12-06 09:36:07 -0300

    feat: update lualine theme and remove useless dependencies

--- nvim/lua/main/packer.lua
+++ nvim/lua/main/packer.lua
@@ -1,19 +1,17 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
   use 'wbthomason/packer.nvim'
-  use 'dracula/vim'
-  use {'neoclide/coc.nvim', branch = 'release'}
+  use 'dracula/vim' -- Theme
   use {
       'nvim-lualine/lualine.nvim',
       requires = { 'kyazdani42/nvim-web-devicons', opt = true }
   }
   use {
       'nvim-telescope/telescope.nvim', tag = '0.1.0',
-    -- or                            , branch = '0.1.x',
       requires = { {'nvim-lua/plenary.nvim'} }
   }
+  -- Flutter plugins
   use {'akinsho/flutter-tools.nvim', requires = 'nvim-lua/plenary.nvim'}
   use {'reisub0/hot-reload.vim'}
 
