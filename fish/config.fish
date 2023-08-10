efc07fd0e12c4d09942ad8a202eb10c1b552dfee
Thales Gelinger <thalesgelinger@gmail.com>
2023-08-10 10:02:17 -0300

    update

--- fish/config.fish
+++ fish/config.fish
@@ -1,16 +1,58 @@
+# Themes
+set -g theme_nerd_fonts yes
+set -g theme_powerline_fonts no
+set -g theme_color_scheme dracula
+
+# PATH
+
+set -gx PATH /usr/local/bin  $PATH
+set -gx PATH /usr/sbin  $PATH
+set -gx PATH /opt/homebrew/bin $PATH
+set -gx PATH $HOME/.local/bin $PATH
+set -gx PATH $HOME/.jenv/bin $PATH
+
+set -gx PATH $ANDROID_HOME/tools $PATH
+set -gx PATH $ANDROID_HOME/emulator $PATH
+set -gx PATH $ANDROID_HOME/tools/bin $PATH
+set -gx PATH $ANDROID_HOME/platform-tools $PATH
+
+set -gx PATH /usr/bin:/bin:/usr/sbin:/sbin $PATH 
+set -gx PATH /Users/tgelin01/.local/bin $PATH 
+
+set -gx PATH "/opt/homebrew/opt/llvm/bin" $PATH
+
+set -gx PATH $HOME/.maestro/bin $PATH
+
+ 
+set ANDROID_HOME /Users/tgelin01/Library/Android/sdk
+set ANDROID_SDK_ROOT /Users/tgelin01/Library/Android/sdk
+
+set -gx FLUTTER_ROOT $(asdf where flutter) 
+set -gx PATH $(asdf where flutter)/bin $PATH
+
+set XDG_CONFIG_HOME $HOME/.config
+set XDG_DATA_HOME $HOME/.local/share
+set XDG_CACHE_HOME $HOME/.cache
 
 
+# ALIAS
 alias intel "arch -x86_64"
-alias fishrc "nvim ~/.config/fish/config.fish"
-alias fishup "source ~/.config/fish/config.fish"
 
 alias ls "exa -la --icons"
-alias g "git"
-alias y "yarn"
-alias iy "intel yarn"
+alias cat "bat"
 
-set PATH /usr/local/bin /usr/sbin $PATH
-set PATH /opt/homebrew/bin $PATH
+alias vim "nvim"
+
+alias gc "git co (git branch -a | fzf | awk '{\$1=\$1};1')"
+
+alias emu "~/.config/scripts/emu"
+alias simu "~/.config/scripts/simu"
+
+alias systrace "$ANDROID_HOME/platform-tools/systrace/systrace.py --time=10 -o trace.html sched gfx view -a "
+
+alias cleanmetro "rm -rf node_modules; yarn; watchman watch-del-all; rm -fr $TMPDIR/haste-map-*; rm -rf $TMPDIR/metro-cache; yarn start -- --reset-cache;"
+alias reset "rm -rf node_modules; yarn; cd ios; pod deintegrate; pod install; cd ..;"
+
 
 if test -d (brew --prefix)"/share/fish/completions"
     set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/completions
@@ -20,30 +62,14 @@ if test -d (brew --prefix)"/share/fish/vendor_completions.d"
     set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
 end
 
-set -g theme_nerd_fonts yes
-set -g theme_powerline_fonts no
 
-set PATH $HOME/.jenv/bin $PATH
+set -gx LDFLAGS "-L/opt/homebrew/opt/llvm/lib"
+set -gx CPPFLAGS "-I/opt/homebrew/opt/llvm/include"
 
-set PATH /usr/bin:/bin:/usr/sbin:/sbin $PATH 
-set PATH /Users/tgelin01/.local/bin $PATH 
-
-set ANDROID_HOME /Users/tgelin01/Library/Android/sdk
-set ANDROID_SDK_ROOT /Users/tgelin01/Library/Android/sdk
-
-set PATH $ANDROID_HOME/tools $PATH
-set PATH $ANDROID_HOME/emulator $PATH
-set PATH $ANDROID_HOME/tools/bin $PATH
-set PATH $ANDROID_HOME/platform-tools $PATH
-
-
-set -g theme_color_scheme dracula
-
-alias cleanmetro "rm -rf node_modules; yarn; watchman watch-del-all; rm -fr $TMPDIR/haste-map-*; rm -rf $TMPDIR/metro-cache; yarn start -- --reset-cache;"
-alias reset "rm -rf node_modules; yarn; cd ios; pod deintegrate; pod install; cd ..;"
-
-
+# ASDF
 . ~/.asdf/plugins/java/set-java-home.fish
+. /opt/homebrew/opt/asdf/libexec/asdf.fish
 
-source /opt/homebrew/opt/asdf/libexec/asdf.fish
 tmux source-file ~/.config/tmux/.tmux.conf
+source /opt/homebrew/opt/asdf/libexec/asdf.fish
+zoxide init fish | source
---
+++ scripts/test.sh
@@ -0,0 +1,5 @@
+
+
+function test 
+    echo "Hello Script"
+end
