1a55b7b4927ed3e8fea91b97f5db6a54950f07a2
Thales Gelinger <thalesgelinger@gmail.com>
2024-10-09 09:56:06 -0300

    update and add aerospace

--- nvim/lua/plugins/comment.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "terrortylor/nvim-comment",
-    config = function()
-        require('nvim_comment').setup()
-        -- Comment
-        vim.keymap.set("n", '<leader>/', '<cmd>CommentToggle<CR>')
-        vim.keymap.set("v", '<leader>/', '<cmd>CommentToggle<CR>')
-    end
-}
